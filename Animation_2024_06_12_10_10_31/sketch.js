464c071c4c7d80f549a1a4ed8dde39e4e3574369
Khadija2104 <144886400+Khadija2104@users.noreply.github.com>
2024-06-12 14:53:08 +0400

    Add files via upload

---
+++ Animation_2024_06_12_10_10_31/sketch.js
@@ -0,0 +1,47 @@
+var numFrames = 5;
+
+var frame = 0
+
+var images = new Array(numFrames);
+
+
+
+function preload() {
+
+    images[0] = loadImage("image0.jpeg");
+
+    images[1] = loadImage("image1.jpeg");
+
+    images[2] = loadImage("image2.jpeg");
+
+    images[3] = loadImage("image3.jpeg");
+
+    images[4] = loadImage("image4.jpeg");
+
+    
+
+}
+
+function setup() {
+
+    createCanvas(600, 500);
+
+    frameRate(5);
+
+    background(255);
+
+}
+
+
+
+function draw() {
+
+background(255);
+
+frame++;
+
+if (frame == numFrames) frame = 0;
+
+image(images[frame], mouseX - 75, mouseY - 100);
+
+}
